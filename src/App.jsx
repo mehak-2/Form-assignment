b5b773feb785e1daa35fbc82f95f7547fc512ebd
mehak-2 <Kangmehak167@gmail.com>
2024-11-08 21:02:33 +0530

    Form-assignment code
    Form-assignment code

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  closestCorners,
+} from "@dnd-kit/core";
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+
+import {  Box } from "./components/Box/Box";
+import { Input } from "./components/Input/Input";
+
+import "./App.css";
+
+export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Wake Up at 7" },
+    { id: 2, title: "Drink Water" },
+    { id: 3, title: "Walk Regularly" },
+    { id: 4, title: "Do Revision" },
+  ]);
+
+  const addTask = (title) => {
+    setTasks((tasks) => [...tasks, { id: tasks.length + 1, title }]);
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const getTaskPos = (id) => tasks.findIndex((task) => task.id === id);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id === over.id) return;
+
+    setTasks((tasks) => {
+      const originalPos = getTaskPos(active.id);
+      const newPos = getTaskPos(over.id);
+
+      return arrayMove(tasks, originalPos, newPos);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>TODO APP</h1>
+      <Input onSubmit={addTask} />
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCorners}
+        onDragEnd={handleDragEnd}
+      >
+        <Box id="toDo" tasks={tasks} />
+      </DndContext>
+    </div>
+  );
+}
